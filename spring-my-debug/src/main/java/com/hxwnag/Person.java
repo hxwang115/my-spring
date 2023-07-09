c955b7692df5152407aa78e29a9695339757c964
hxwang115 <hxwang1_15@163.com>
2023-07-09 23:57:54 +0800

    编译

---
+++ spring-my-debug/src/main/java/com/hxwnag/Person.java
@@ -0,0 +1,33 @@
+package com.hxwnag;
+
+/**
+ * <H1> Person </H1>
+ * <p>  </p>
+ * <p>
+ * 创建日期: 2023/7/9 <p>
+ * 创建时间: 23:34
+ *
+ * @author hxwang
+ * @version 1.0
+ */
+
+public class Person {
+	private String name;
+	private int age;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+}
